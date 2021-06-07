8fb49636ff5b43d94a6a9b31da0395c99b27d328
Paul Stahlhofen <paul.stahlhofen@uni-bielefeld.de>
2021-06-07 12:47:38 +0200

    Moved msp files to inst/msp_examples so they will be automatically installed with the package Added a script to run the mbWorkflow with processed msp data

---
+++ inst/msp_examples/run_msp_example.R
@@ -0,0 +1,9 @@
+library(RMassBank)
+w <- newMsmsWorkspace()
+files <- list.files('.', '.msp', full.names=TRUE)
+w@files <- files
+loadList('./Compoundlist.csv')
+loadRmbSettings('./RMB_options.ini')
+w <- msmsWorkflow(w, readMethod='msp', filetable='./Filelist.csv', mode='pH', steps=1, archivename='msp_archive')
+mb <- newMbWorkspace(w)
+mb <- mbWorkflow(mb)
