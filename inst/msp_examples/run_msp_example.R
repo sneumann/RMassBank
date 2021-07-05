f840711840f0fb360cbd348acfa3e7842f1ffde9
Michele Stravs <stravsm@imsb.biol.ethz.ch>
2021-07-05 11:34:22 +0200

    moved data to RMassBankData, adapted msp workflow

--- inst/msp_examples/run_msp_example.R
+++ inst/msp_examples/run_msp_example.R
@@ -1,12 +1,14 @@
 library(RMassBank)
 w <- newMsmsWorkspace()
-files <- list.files('.', '.msp', full.names=TRUE)
+files <- list.files(system.file('msp_examples', package="RMassBankData"), '.msp', full.names=TRUE)
 w@files <- files
-loadList('./Compoundlist.csv')
-loadRmbSettings('./RMB_options.ini')
-w <- msmsWorkflow(w, readMethod='msp', filetable='./Filelist.csv', mode='pH', steps=1, archivename='msp_archive')
+loadList(system.file('msp_examples/Compoundlist.csv', package="RMassBankData"))
+loadRmbSettings(system.file('msp_examples/RMB_options.ini', package="RMassBankData"))
+w <- msmsWorkflow(w, readMethod='msp', 
+                  filetable=system.file('msp_examples/Filelist.csv', package="RMassBankData"),
+                  mode='pH', steps=1, archivename='msp_archive')
 mb <- newMbWorkspace(w)
 #mb <- mbWorkflow(mb)
 mb <- resetInfolists(mb)
-mb <- loadInfolists(mb, 'infolists')
+mb <- loadInfolists(mb, system.file('infolists', package="RMassBankData"))
 mb <- mbWorkflow(mb, filter=FALSE)
