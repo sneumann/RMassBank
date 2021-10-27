808be3f4badb34357bad2cf20fb0f3e92c3ec793
Paul Stahlhofen <paul.stahlhofen@uni-bielefeld.de>
2021-10-27 17:17:13 +0200

    Started to document `RmbSpectrum2`

--- R/SpectrumClasses.R
+++ R/SpectrumClasses.R
@@ -2,6 +2,62 @@
 #' @importFrom Biobase classVersion
 #' @import S4Vectors
 
+#' RMassBank Representation of an MSMS Spectrum
+#'
+#' This extends the \code{Spectrum2} class of the \code{MSnbase} package and
+#' introduces further slots that are used to store information during
+#' the \code{RMassBank} workflow.
+#'
+#' @slot satellite logical
+#' If \code{TRUE}, the corresponding peak was removed as satellite.
+#' @slot low logical
+#' If \code{TRUE}, the corresponding peak was removed
+#' because it failed the intensity? cutoff.
+#' @slot rawOk logical
+#' If \code{TRUE} the peak passed satellite and intensity? cutoff removal.
+#' @slot good ?
+#' @slot mzCalc numeric
+#' The mz value calculated from the found formula (if any)
+#' @slot formula character
+#' The formula found for each peak.
+#' \code{Rcdk} is used for formula-fitting?
+#' @slot dbe numeric
+#' The number of double bond equivalents.
+#' This is calculated from the found formula (if any)
+#' @slot formulaCount integer
+#' The number of different formulae found for each peak.
+#' @slot formulaSource ?
+#' @slot dppm numeric
+#' The ppm deviation of the mz value from the found formula (if any).
+#' @slot dppmBest numeric
+#' The ppm deviation of the mz value from the best formula found. (What exactly happens if multiple formulae were found?)
+#' @slot ok logical one-element vector
+#' If this is \code{TRUE}, the spectrum was successfully processed
+#' with at least one resulting peak.
+#' Otherwise, one of the following cases applies:
+#' \begin{itemize}
+#' \item All peaks failed the intensity cutoff
+#' i.e. the whole spectrum contains low intensity peaks, only.
+#' \item All peaks were marked as satellites.
+#' \item All peaks in the spectrum have a lower intensity than the value
+#' given in the \code{specOkLimit} filter setting. (see the \code{RMassBank}
+#' vignette or the documentation of \code{\link{analyzeMsMs}})
+#' \item The precursor ion formula is invalid (see \code{\link{is.valid.formula}})
+#' \item The spectrum is empty.
+#' \item No molecular formula could be found for any of the peaks.
+#' \item All peaks failed the \code{dbeMinLimit} criterion. (see the
+#' \code{RMassBank} vignette or the documentation of \code{\link{analyzeMsMs}})
+#' \end{itemize}
+#' @slot info list
+#' Spectrum identifying information
+#' (collision energy, resolution, collision mode) from the \code{spectraList}
+#' @slot properties data.frame
+#' This is used as a flexible placeholder to store additional properties
+#' for each peak throughout the workflow. After the last step of the
+#' \code{mbWorkflow}, this will typically contain columns \code{mzRaw},
+#' \code{noise}, \code{formulaMultiplicity}, \code{bestMultiplicity}
+#' and \code{filterOK}. However, new columns may be added on demand
+#' (see \code{\link{property-set}})
 #' @exportClass RmbSpectrum2
 .RmbSpectrum2 <- setClass("RmbSpectrum2",
 		representation = representation(
